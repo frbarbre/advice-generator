68017aca69698f0b834905e0315e6523cb8d11a5
frbarbre <fr.barbre@gmail.com>
2023-06-25 04:08:30 +0200

    added comments and animations

--- app/components/AdviceCard.jsx
+++ app/components/AdviceCard.jsx
@@ -1,34 +1,65 @@
+import { motion } from "framer-motion";
+
 export default function AdviceCard(props) {
   return (
-    <div className="bg-neutral-medium-gray flex flex-col justify-center items-center max-w-[500px] w-[80%] pt-8 px-8 rounded-xl">
-      <h2 className="uppercase text-primary-neon font-extrabold tracking-[0.2em] text-[12px] pb-5">
-        Advice #{props.slip === undefined ? "?" : props.slip.id}
-      </h2>
-      {props.slip === undefined ? (
-        <h1 className="pb-8 text-red-500 text-[18px] font-extrabold">
-          Bad Gateway: Please try again
-        </h1>
-      ) : (
-        <h1 className="text-primary-white font-extrabold text-[24px] text-center pb-8">
-          "{props.slip.advice}"
-        </h1>
-      )}
-      <img
-        className="hidden sm:block"
-        src="/pattern-divider-desktop.svg"
-        alt="divider"
-      />
-      <img
-        className="sm:hidden"
-        src="/pattern-divider-mobile.svg"
-        alt="divider"
-      />
-      <div
-        className="bg-primary-neon p-4 rounded-full w-max hover:shadow-btn hover:shadow-primary-neon transition-all translate-y-[50%] cursor-pointer"
-        onClick={() => props.fetchData(props.randomizer())}
+    <div className="bg-neutral-medium-gray max-w-[500px] w-[80%] pt-8 px-8 rounded-xl">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+        className="flex flex-col justify-center items-center"
       >
-        <img src="/icon-dice.svg" alt="dice-btn" />
-      </div>
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.8, duration: 0.5 }}
+          exit={{ opacity: 0, y: 0 }}
+          className="uppercase text-primary-neon font-extrabold tracking-[0.2em] text-[12px] pb-5"
+        >
+          Advice #{props.slip === undefined ? "?" : props.slip.id}
+        </motion.h2>
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          exit={{ opacity: 0, y: 0 }}
+        >
+          {props.slip === undefined ? (
+            <h1 className="pb-8 text-red-500 text-[18px] font-extrabold">
+              Bad Gateway: Please try again
+            </h1>
+          ) : (
+            <h1 className="text-primary-white font-extrabold text-[24px] text-center pb-8">
+              "{props.slip.advice}"
+            </h1>
+          )}
+        </motion.div>
+        <motion.img
+          initial={{ opacity: 0, scaleX: 0 }}
+          animate={{ opacity: 1, scaleX: 1 }}
+          exit={{ opacity: 0, scaleX: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="hidden sm:block"
+          src="/pattern-divider-desktop.svg"
+          alt="divider"
+        />
+        <motion.img
+          initial={{ opacity: 0, scaleX: 0 }}
+          animate={{ opacity: 1, scaleX: 1 }}
+          exit={{ opacity: 0, scaleX: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="sm:hidden"
+          src="/pattern-divider-mobile.svg"
+          alt="divider"
+        />
+        <div
+          className="bg-primary-neon p-4 rounded-full w-max hover:shadow-btn hover:shadow-primary-neon transition-all translate-y-[50%] cursor-pointer"
+          onClick={() => props.fetchData(props.randomizer())}
+        >
+          <img src="/icon-dice.svg" alt="dice-btn" />
+        </div>
+      </motion.div>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -6,42 +6,45 @@ import { AdviceCard } from "./components";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
+  // State that is set to hold the incoming API data
   const [advice, setAdvice] = useState([]);
+  // State that checks for whether to data has loaded or not.
   const [isLoaded, setIsLoaded] = useState(false);
-  const [error, setError] = useState(true);
-  const [random] = useState(randomizer);
 
-  async function fetchData(randomId) {
-    randomizer();
-    setIsLoaded(false);
-    setError(true);
-    const { data } = await axios.get(
-      `https://api.adviceslip.com/advice/${randomId}`
-    );
-    setIsLoaded(true);
-    setError(false);
-    setAdvice(data);
-    console.log(advice);
-  }
-
-  useEffect(() => {
-    fetchData(random);
-  }, []);
-
+  // Function that generates a random number between 1 and 224 (the amount of objects in the Advice API)
   function randomizer() {
     let randomNumber = Math.floor(Math.random() * 226) + 1;
 
     return randomNumber;
   }
 
+  // Fetching data using axios, while generating a new random number every time the function is run.
+  async function fetchData(randomId) {
+    randomizer();
+    setIsLoaded(false);
+    const { data } = await axios.get(
+      `https://api.adviceslip.com/advice/${randomId}`
+    );
+    setTimeout(() => {
+      setIsLoaded(true);
+      setAdvice(data);
+    }, 500);
+  }
+
+  // Running the fetchData() function on mount
+  useEffect(() => {
+    fetchData(randomizer());
+  }, []);
+
   return (
     <main className="flex min-h-screen w-screen items-center justify-center bg-neutral-dark-gray">
       <AnimatePresence>
-        {isLoaded && error === false ? (
+        {isLoaded && (
           <motion.div
-            initial={{ opacity: 0, y: 0 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 0 }}
+            initial={{ x: -20, scaleX: 0 }}
+            animate={{ x: 0, scaleX: 1 }}
+            transition={{ type: "spring", duration: 0.6 }}
+            exit={{ x: 0, scaleX: 0 }}
             className="flex items-center justify-center"
           >
             <AdviceCard
@@ -50,10 +53,6 @@ export default function Home() {
               randomizer={randomizer}
             />
           </motion.div>
-        ) : (
-          <p className="font-extrabold text-[18px] text-primary-neon">
-            Loading...
-          </p>
         )}
       </AnimatePresence>
     </main>
